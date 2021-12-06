468e4546c8b73a5a0f2518f555380600d5ae5cc6
boghsteren <boghsteren@gmail.com>
2021-12-06 15:06:03 +0100

    Removed image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "simply-digital 2022",
+    title: "Simply Digital",
   },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"],
 };
